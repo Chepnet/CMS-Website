eddbd6130d96757d2e54c8ea4f0f0f20729963ce
Chepnet <49672106+Chepnet@users.noreply.github.com>
2021-04-27 13:16:38 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Content_Management_System.Models;
+using PagedList;
+using Microsoft.EntityFrameworkCore;
+
+namespace Content_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            List<Training> model = _context.Trainings.ToList();
+
+            return View(model);
+        }
+
+        
+        public IActionResult Applicants()
+        {
+            List<TrainingApplication> model = _context.TrainingApplications.ToList();
+
+            return View(model);
+        }
+        public IActionResult Viewdetails(int? id)
+        {
+            Training model = _context.Trainings.Find(id);
+            return View(model);
+
+        }
+        public ActionResult Search(string option, string search, int? pageNumber, string sort)
+        {
+
+            //if the sort parameter is null or empty then we are initializing the value as descending name  
+            ViewBag.sortByEventName = string.IsNullOrEmpty(sort) ? "descending Event" : "";
+            //if the sort value is gender then we are initializing the value as descending gender  
+            ViewBag.SortByTrainerName = sort == "TrainerName" ? "descending TrainerName" : "TrainerName";
+            ViewBag.SortByEventName = sort == "EventName" ? "descending EventName" : "EventName";
+            ViewBag.SortByCost = sort == "Cost" ? "descending Cost" : "Cost";
+            ViewBag.SortByTrainingTime = sort == "TrainingTime" ? "descending TrainingTime" : "TrainingTime";
+            //here we are converting the db.Students to AsQueryable so that we can invoke all the extension methods on variable records.  
+            var records = _context.Trainings.AsQueryable();
+
+            if (option == "TrainingTime")
+            {
+                records = records.Where(x => x.TrainingTime.ToString() == search || search == null);
+            }
+            else if (option == "TrainerName")
+            {
+                records = records.Where(x => x.TrainerName == search || search == null);
+            }
+            else
+            {
+                records = records.Where(x => x.EventName.StartsWith(search) || search == null);
+            }
+
+            switch (sort)
+            {
+
+                case "descending EventName":
+                    records = records.OrderByDescending(x => x.EventName);
+                    break;
+                case "descending Cost":
+                    records = records.OrderByDescending(x => x.Cost);
+                    break;
+
+                case "descending TrainerName":
+                    records = records.OrderByDescending(x => x.TrainerName);
+                    break;
+                case "descending TrainingTime":
+                    records = records.OrderByDescending(x => x.TrainingTime);
+                    break;
+
+                case "TrainerName":
+                    records = records.OrderBy(x => x.TrainerName);
+                    break;
+
+                case "TrainingTime":
+                    records = records.OrderBy(x => x.TrainingTime);
+                    break;
+
+                case "Cost":
+                    records = records.OrderBy(x => x.Cost);
+                    break;
+                default:
+                    records = records.OrderBy(x => x.EventName);
+                    break;
+            }
+
+            return View(records.ToPagedList(pageNumber ?? 1, 20));
+        }
+        public IActionResult add(int? TrainingId, string TrainerName, string EventName, string Description, DateTime? TrainingTme, decimal? Cost)
+        {
+
+
+            Training trainings = new Training();
+
+
+            if (ModelState.IsValid)
+            {
+                TrainingTme = DateTime.Now;
+                Cost = 0;
+                trainings.TrainerName = TrainerName;
+                trainings.EventName = EventName;
+                trainings.Description = Description;
+                trainings.TrainingTime = (DateTime)TrainingTme;
+                trainings.Cost = (decimal)Cost;
+
+
+                if (TrainerName != null)
+                {
+                    _context.Trainings.Add(trainings);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(trainings);
+        }
+        public IActionResult ApplicationDetails(int? id)
+        {
+            TrainingApplication model = _context.TrainingApplications.Find(id);
+            return View(model);
+
+        }
+        public ActionResult Edit(int? id)
+        {
+            
+            Training  farmersTraining = _context.Trainings.Find(id);
+            if (farmersTraining == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(farmersTraining);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int? TrainingId, string TrainerName, string EventName, string Description, DateTime? TrainingTme, decimal? Cost)
+        {
+            Training trainings = new Training();
+           TrainingTme = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                TrainingTme = DateTime.Now;
+                Cost = Cost;
+                _context.Entry(trainings).State = EntityState.Modified;
+                try
+                {
+                    _context.SaveChanges();
+                }
+                catch {; }
+                
+                return RedirectToAction("Index");
+            }
+            return View(trainings);
+        }
+        public ActionResult Delete(int id)
+        {
+            Training farmersTraining = _context.Trainings.Find(id);
+            _context.Trainings.Remove(farmersTraining);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+using Content_Management_System.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Content_Management_System
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+           
+            services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
